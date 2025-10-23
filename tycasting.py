24ea0128a4f6e96ca2f8b2f6e2f9e15f39b37382
Gonçalo Trindade <josegoncalosantostrindade@gmail.com>
2025-10-23 21:50:45 +0100

    intro, variables & typecasting

---
+++ tycasting.py
@@ -0,0 +1,36 @@
+# Explicit
+name = 'Gonçalo'
+age = 20
+gpa = 1.9
+student = True
+
+'''
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(student))
+'''
+
+'''
+age = float(age)
+print(age)
+print(type(age))
+
+gpa = int(gpa)
+print(gpa)
+print(type(gpa))
+
+student = str(student)
+print(student)
+print(type(student))
+
+age = bool(age) # if age = 0 the output will be False
+print(age)
+'''
+
+# Implicit
+x = 2
+y = 2.0
+
+x = x / y
+print(x)
---
+++ variables.py
@@ -0,0 +1,43 @@
+# Strings
+first_name = 'Gonçalo'
+food = 'pizza'
+email = 'Gon123@fake.com'
+
+'''
+print(f'Hello {first_name}')
+print(f'You like {food}')
+print(f'Your email is: {email}')
+'''
+
+# Integers
+age = 20
+quantity = 3
+num_of_students = 30
+
+'''
+print(f'You are {age} years old')
+print(f'You are buying {quantity} items')
+print(f'Your class has {num_of_students} students')
+'''
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+'''
+print(f'The price is ${price}')
+print(f'Your gpa is: {gpa}')
+print(f'You ran {distance}km')
+'''
+
+# Boolean
+is_student = True
+for_sale = False
+is_online = True
+
+'''
+print(f'Are you a student? {is_student}')
+print(f'This item is for sale? {for_sale}')
+print(f'Are you online? {is_online}')
+'''
