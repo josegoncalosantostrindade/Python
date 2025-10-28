8780e927e19d9bf54c97da91f9cd81c64b9f28da
Gonçalo Trindade <josegoncalosantostrindade@gmail.com>
2025-10-28 16:34:47 +0000

    ifstatements, calculator, temperature and weight convertions

---
+++ ifstatements.py
@@ -0,0 +1,31 @@
+# Theory
+'''
+age = int(input('Enter your age: '))
+
+if age >= 100:
+    print('You are too old to sign up!')
+elif age >= 18:
+    print('Your are now signed up!')
+elif age < 0:
+    print('You have not been born yet!')
+else:
+    print('You must be 18+ to sign up!')
+'''
+
+# Exercise: Food
+'''
+response = input('Would you like food? (Y/N): ')
+
+if response == 'Y':
+    print('Have some food!')
+else:
+    print('No food for you!')
+'''
+
+# Exercise: 
+name = input('Enter your name: ')
+
+if name == '':
+    print('You did not type in your name!')
+else:
+    print(f'Hello {name}')
