48c29cfb29187bfcef5356c5bd91532b807da785
Gonçalo Trindade <josegoncalosantostrindade@gmail.com>
2025-11-02 22:04:18 +0000

    logical operators & conditional expressions

---
+++ logicaloperators.py
@@ -0,0 +1,21 @@
+temp = 20
+sunny = True
+
+'''
+if temp > 0 and temp < 30:
+    print('The temperature is good')
+else:
+    print('The temperature is bad')
+'''
+
+'''
+if temp <= 0 or temp >= 30:
+    print('The temperature is bad')
+else:
+    print('The temperature is good')
+'''
+
+if not sunny:
+    print('It is cloudy outside')
+else:
+    print('It is sunny outside')
