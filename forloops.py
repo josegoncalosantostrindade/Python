fec9dc01b118da9ac98b74ae50ac74f08f33009a
Gonçalo Trindade <josegoncalosantostrindade@gmail.com>
2025-11-06 16:21:18 +0000

    for loops, nested loops & countdown timer exercise

---
+++ forloops.py
@@ -0,0 +1,20 @@
+'''
+for x in reversed(range(1, 11, 2)):
+    print(x)
+
+print('HAPPY NEW YEAR')
+'''
+
+'''
+credit_card = '1234-5678-9012-3456'
+
+for x in credit_card:
+    print(x)
+'''
+
+for x in range(1, 21):
+    if x == 13:
+        # continue
+        break
+    else:
+        print(x)
---
+++ nestdedloops.py
@@ -0,0 +1,18 @@
+'''
+for x in range (3):
+    for y in range(1, 10):
+        print(y, end='')
+    
+    print()
+'''
+
+# Exercise: Rectangle
+rows = int(input('Enter the number of rows: '))
+columns = int(input('Enter the number of columns: '))
+symbol = input('Enter a symbol to use: ')
+
+for x in range (rows):
+    for y in range(columns):
+        print(symbol, end='')
+    
+    print()
