1270dc8f7a280feaf279ed9ae0b9514e74606a9d
Gonçalo Trindade <josegoncalosantostrindade@gmail.com>
2025-11-10 07:24:31 +0000

    random numbers, guessing numbers & rock, paper and scissors

---
+++ randomnumbers.py
@@ -0,0 +1,37 @@
+import random
+
+'''
+low = 1
+high = 100
+options = ('rock', 'paper', 'scissors')
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+print(help(random))
+number = random.randint(low, high)
+number = random.random()
+option = random.choice(options)
+random.shuffle(cards)
+
+print(cards)
+'''
+
+
+# Exercise: Find The Number
+low = 1
+high = 100
+guesses = 0
+number = random.randint(low, high)
+
+while True:
+    guess = int(input(f'Enter a number between ({low} - {high}): '))
+    guesses += 1
+
+    if guess < number:
+        print(f'{guess} is too low')
+    elif guess > number:
+        print(f'{guess} is too high')
+    else:
+        print(f'{guess} is correct')
+        break
+
+print(f'This round took you {guesses} guesses')
