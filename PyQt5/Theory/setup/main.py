b5e83db53c6ffa92f46b03a8377ed50662f0ffe4
Gonçalo Trindade <josegoncalosantostrindade@gmail.com>
2025-11-28 13:40:13 +0000

    setup, labels & images

--- PyQt5/Theory/setup/main.py
+++ PyQt5/Theory/setup/main.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('My Cool First GUI')
+        self.setGeometry(500, 250, 500, 500)
+        self.setWindowIcon(QIcon('obsidian.png'))
 
 def main():
     app = QApplication(sys.argv)
