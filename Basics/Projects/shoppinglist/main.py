5fb505b21fbef2c4338013db56e7590a38cbbf93
Gonçalo Trindade <josegoncalosantostrindade@gmail.com>
2025-11-28 15:31:57 +0000

    Shopping List Program (Basics)

--- Basics/Projects/shoppinglist/main.py
+++ Basics/Projects/shoppinglist/main.py
@@ -1,46 +1,61 @@
-file_path = 'Basics/Projects/shoppinglist/shop.txt'
+def get_file():
+    # print('!!!In Get File!!!')
 
-'''try:
+    global shopping_list
+    shopping_list = []
     with open(file_path, 'r') as file:
-        content = file.read()
-        # print(content)
-except FileNotFoundError:
-    print('File not found')'''
+        for line in file:
+            shopping_list.append(line.strip())
 
-shopping_list = []
-is_running = True
+    # print('!!!Out Get File!!!')
 
-while is_running:
-    item_input = input('Enter an item for you shopping list (q to quit): ').capitalize()
+def main():
+    global file_path
+    file_path = '/Users/jgtrindade/Desktop/Curriculo/Python/Basics/Projects/shoppinglist/shop.txt'
 
-    if not item_input.lower().strip() == 'q':
-        if not all(char.isalpha() for char in item_input.split()):
-            print('!!!In Verification Item!!!')
+    get_file()
+    is_running = True
+    temp_shopping_list = []
 
-            print('Invalid item!')
+    while is_running:
+        item_input = input('Enter an item for you shopping list (q to quit): ')
 
-            print('!!!Out Verification Item!!!')
-            continue
+        if not item_input.lower().strip() == 'q':
+            if not all(char.isalpha() for char in item_input.split()):
+                # print('!!!In Verification Item!!!')
+
+                print('Invalid item!')
+
+                # print('!!!Out Verification Item!!!')
+                continue
+            else:
+                item_input.capitalize()
+                # print('!!!In Append To List!!!')
+
+                temp_shopping_list.append(item_input)
+
+                # print('!!!Out Append To List!!!')
+                continue
         else:
-            print('!!!In Append To List!!!')
+            pass
 
-            shopping_list.append(item_input)
+        # print(temp_shopping_list)
+        for item in temp_shopping_list:
+            # print('!!!In Append To File!!!')
 
-            print('!!!Out Append To List!!!')
-            continue
-    else:
-        pass
+            if item in shopping_list:
+                pass
+            else:
+                with open(file_path, 'a') as file:
+                    file.write(f'{item}\n')
 
-    print(shopping_list)
+            # print('!!!Out Append To File!!!')
 
-    print('!!!In Append To File!!!')
+        get_file()
+        is_running = False
 
-    for item in shopping_list:
-        print('!!!In Append To File!!!')
+    print('END')
 
-        with open(file_path, 'a') as file:
-            file.write(f'{item}\n')
 
-        print('!!!Out Append To File!!!')
-
-print('END')
+if __name__ == '__main__':
+    main()
