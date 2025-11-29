23f70c37860ba897a5098d2ec68321b0b7c9319d
Gonçalo Trindade <josegoncalosantostrindade@gmail.com>
2025-11-29 23:19:32 +0000

    broadcasting, functions & aggregate functions

---
+++ NumPy/Theory/functions.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# Zeros
+'''array = np.zeros((2, 3, 10))
+print(array)'''
+
+
+# One's
+'''array = np.ones((2, 3, 10))
+print(array)'''
+
+
+# Full
+'''array = np.full((2, 3, 10), 9)
+print(array)'''
+
+
+# Eye
+'''array = np.eye(4)
+print(array)'''
+
+
+# Empty
+'''array = np.empty((2, 3))
+print(array)'''
+
+
+# Arange
+'''array = np.arange(0, 100, 1) # start, end, step - end exclusive
+print(array)'''
+
+
+# Linspace
+array = np.linspace(0, 10, 6) # start, end, num - end exclusive
+print(array)
