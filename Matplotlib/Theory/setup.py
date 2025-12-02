bf6a2318ca02133863a905df1498a99a64305cc3
Gonçalo Trindade <josegoncalosantostrindade@gmail.com>
2025-12-02 11:52:27 +0000

    setup

---
+++ Matplotlib/Theory/setup.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# print(matplotlib.__version__)
+
+x = np.array([2023, 2024, 2025, 2026])
+y = np.array([15, 25, 30, 20])
+
+plt.plot(x, y)
+plt.show()
